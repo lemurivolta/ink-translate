d044007a241749c163d2b32e874de7382c1111a4
mattia <mattia.belletti@gmail.com>
2023-08-17 22:29:08 +0200

    cleanup

--- Editor/FileDict.cs
+++ Editor/FileDict.cs
@@ -6,7 +6,5 @@ namespace LemuRivolta.InkTranslate.Editor
     /// A class used to wrap the concept of double-indexed entities, by filename and by line.
     /// </summary>
     /// <typeparam name="T">The type of the entities indexed</typeparam>
-    public class FileDict<T> : Dictionary<string, Dictionary<int, T>>
-    {
-    }
+    public class FileDict<T> : Dictionary<string, Dictionary<int, T>> { }
 }
---
+++ Third Party Notices.md
@@ -0,0 +1,7 @@
+This package contains third-party software components governed by the license(s) indicated below:
+
+Component Name: XLIFF 2.0 Object Model (NetStandard)
+
+License Type: "MIT"
+
+Provide License Details: [XLIFF 2.0 Object Model (NetStandard)](https://github.com/VladimirRybalko/Xliff.OM.NetStandard/blob/master/LICENSE)
