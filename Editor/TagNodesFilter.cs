decd5ea71d4faf5cf54da5e0de43776533b99938
mattia <mattia.belletti@gmail.com>
2023-08-18 17:29:02 +0200

    chore: some more comments

--- Editor/TagNodesFilter.cs
+++ Editor/TagNodesFilter.cs
@@ -25,6 +25,13 @@ namespace LemuRivolta.InkTranslate.Editor
         private readonly string translationNotePrefix;
         private string mainFilePath;
 
+        /// <summary>
+        /// Create a new tag nodes filter.
+        /// </summary>
+        /// <param name="translationNotePrefix">The prefix used to identify tags that are
+        /// about translations (e.g.: "tnote:").
+        /// The prefix is removed from the saved tag.
+        /// If <c>null</c> or "", no tag is extracted as a translation note.</param>
         public TagNodesFilter(string translationNotePrefix)
         {
             this.translationNotePrefix = translationNotePrefix;
@@ -40,17 +47,21 @@ namespace LemuRivolta.InkTranslate.Editor
         {
             if(string.IsNullOrEmpty(translationNotePrefix))
             {
+                // don't extract tags if no translation note prefix is set
                 return;
             }
 
+            // track whether we're inside a tag
             if (o is Tag tag)
             {
                 insideTag = tag.isStart;
             }
+            // check if the content of the tag is of interest to us
             if (insideTag &&
                 o is Text t &&
                 t.text.StartsWith(translationNotePrefix))
             {
+                // it is: save it!
                 var d = o.debugMetadata;
                 string mainFileRelativePath =
                     d.fileName.MakeInkPathRelative(mainFilePath);
