be63b4c5ae378d0195c398472a4dcac08638bfe9
Mattia Belletti <mbelletti@greenteam.it>
2023-11-09 12:26:02 +0100

    chore: cleanup unused imports

--- Packages/it.lemurivolta.ink-translate/Editor/FileLines.cs
+++ Packages/it.lemurivolta.ink-translate/Editor/FileLines.cs
@@ -1,10 +1,4 @@
-using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Security.Cryptography;
-using System.Text;
-
-using Ink.Parsed;
 
 namespace LemuRivolta.InkTranslate.Editor
 {
